0af5d9f8a5280ba97ce988718051bdb779279a2e
Anne Lie <103604990+ennaayoshi@users.noreply.github.com>
2026-03-09 09:05:09 -0300

    Primeira Lista de Exercícios - Java
    Exercícios feitos: 
    Exercício 1 e 2 (3 na metade) ✔

---
+++ Exercício3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaexsjava;
+import java.util.Scanner;
+
+//@author CAMARGO
+
+public class Exercício3 {
+     public static void main(String[] args) {
+        System.out.println(" - Exercício 3 - ");
+        Scanner scanner = new Scanner(System.in);
+     }
+}
